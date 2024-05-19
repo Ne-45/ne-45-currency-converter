d6f7629ecb0398a2dd09a9e224c636ea96972dac
Ne-45 <159222022+Ne-45@users.noreply.github.com>
2024-05-19 22:22:49 +0500

    created currency converter

---
+++ ne.ts
@@ -0,0 +1,42 @@
+import inquirer from "inquirer";
+
+
+const currency: any ={
+    USD: 1,  
+    INR: 83.30,
+    AED: 3.67,
+    OMR: 0.38,
+    PKR: 277.09
+};
+let user_answer = await inquirer.prompt(
+   [
+     {
+    name: 'from',
+    message:"Enter From Currency",
+    type:'list',
+    choices:['USD', 'INR', 'AED', 'OMR', 'PKR']
+     },
+     {
+        name: 'to',
+        message:"Enter To Currency",
+        type:'list',
+        choices:['USD', 'INR', 'AED', 'OMR', 'PKR']
+     }, 
+     {
+            name: 'amount',
+            message:"Enter Your Amount",
+            type:'number',
+            
+     }
+   ]
+)
+let fromAmount = currency[user_answer.from]
+let toAmount = currency[user_answer.to]
+let amount = user_answer.amount
+let baseAmount = amount / fromAmount
+let convertedAmount = baseAmount * toAmount
+console.log(convertedAmount);
+
+
+
+
